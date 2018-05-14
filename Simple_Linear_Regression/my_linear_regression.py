d9d2e9c354e4fa08dd9db1f8f6ff6018731ebdbb
ceesim <ceesim@126.com>
2018-05-14 09:32:08 +0800

    new

--- Simple_Linear_Regression/my_linear_regression.py
+++ Simple_Linear_Regression/my_linear_regression.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
+#导入数据集合
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
+#将数据分割为训练数据和测试数据
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
@@ -20,3 +22,34 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting Simple Linear Regression to the Training set
+#对SLR训练集合进行适配
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+
+# Predicting the Test set results
+# 预测测试集合
+y_pred = regressor.predict(X_test)
+
+# Visualising the Training set results
+#可视化训练集合的结果
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience(Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Test set results
+#可视化训练集合的结果
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_test, regressor.predict(X_test), color = 'blue')
+plt.title('Salary vs Experience(Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
